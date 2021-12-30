c1dcd682c4cf87da6f9b959f2ad5558029967862
Harshal <harshal.2797@gmail.com>
2021-12-31 00:07:08 +0530

    Day 5. Problem 189, O(1) space

--- strStr.js
+++
@@ -1,9 +0,0 @@
-var strStr = function (haystack, needle) {
-  if (needle === "") return 0; //If needle is an empty return 0
-  if (haystack.includes(needle)) {
-    //If haystack has the needle proceed further
-    return haystack.indexOf(needle); //Return index where needle begins
-  } else {
-    return -1; //If needle is not a part of the haystack return -1
-  }
-};
